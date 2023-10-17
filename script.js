a681ff2e671e0ee6a2b05423a8efdd3839992335
M. FASRUL FAIS ILMAN <129393218+ilmanfais@users.noreply.github.com>
2023-10-17 21:16:35 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+// membuat program pengecekan user
+
+// function untuk melakukan pengecekan user berdasarkan data user 
+function CekUser(data_username, data_password){
+    let user = [
+        ['admin', 'admin123'],
+        ['santri', 'santri123'],
+        ['hasan', 'hasangenteng'],
+    ]
+
+    for (user of user){
+        let u = user[0] // ini adalah username
+        let p = user[1] // Ini adalah password
+
+        if (u == data_username && p == data_password) 
+        {
+            window.location = "home.html"
+        }
+    }
+}
+
+function Login() {
+
+    let username = document.getElementById('username').value
+
+    let password = document.getElementById('password').value
+
+    if (username.length < 1 || password.length < 1)
+    {
+        alert("Isi username atau password terlebih dahulu")
+    }
+
+    else if(password.length < 8) 
+    {
+        alert("password minimal 8 karakter")
+    }
+
+    else{
+        CekUser(username, password)
+    }
+
+}
+
+document.getElementById('simpan').addEventListener('click', Login)
